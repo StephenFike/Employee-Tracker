ce0041caf44d2b82dd9cb72f30c9c732cd781072
Stephen Fike <sfike98@gmail.com>
2022-05-08 18:48:11 -0500

    working on fixing schema file

--- db/schema.sql
+++ db/schema.sql
@@ -3,12 +3,12 @@ CREATE DATABASE employeedb;
 USE employeedb;
 
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY AUTO_INCREMEMT,
+    id INTEGER AUTO_INCREMEMT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INTEGER PRIMARY KEY AUTO_INCREMEMT,
+    id INTEGER AUTO_INCREMEMT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER,
@@ -16,11 +16,11 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employee (
-    id INTEGER PRIMARY KEY AUTO_INCREMEMT,
+    id INTEGER AUTO_INCREMEMT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER,
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
-)
+);
